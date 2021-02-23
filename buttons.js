37e844d421d8bd37a7baec28e0c1ecb509d8e10b
Chin Zhe Jing <zhechin.zj@gmail.com>
2021-02-23 16:10:07 +0800

    update

--- buttons.js
+++ buttons.js
@@ -83,8 +83,8 @@ current = option.index;
 if (metrics[current-1] === "mentions" || metrics[current-1] === "retweets"){
     choose = 1
 }
-
 show_images(choose)
+
 
 options2.forEach((option, i) => (option.index = i + 1));
 options2.forEach(option =>
@@ -98,7 +98,98 @@ show_images(choose)
 
 }));
 
-
-
 }));
 
+d3.csv("csv/new_top_hashtags.csv", function(error, myData) {
+    if (error) {
+        console.log("Had an error loading file.");
+    }
+    // We'll be using simpler data as values, not objects.
+    var myArray = [];
+    myData.forEach(function(d, i){
+        // Add a new array with the values of each:
+        myArray.push([d.Brand, d.Hashtags, d.Tweets]);
+    });
+    console.log(myData);
+    console.log(myArray);
+    
+    // You could also have made the new array with a map function!
+    //using colors and fonts from the UNICEF Style Guide
+    var table = d3.select("#table1").append("table")
+    .attr("id", "keywords")
+    .attr("cellspacing", 0)
+    .attr("cellpadding", 0);
+    var header = table.append("thead").append("tr");
+    header
+            .selectAll("th")
+            .data(["Brands", "Hashtags", "Tweets"])
+            .enter()
+            .append("th")
+            .append("span")
+            .text(function(d) { return d; });
+    var tablebody = table.append("tbody");
+    rows = tablebody
+            .selectAll("tr")
+            .data(myArray)
+            .enter()
+            .append("tr");
+    // We built the rows using the nested array - now each row has its own array.
+    cells = rows.selectAll("td")
+        // each row has data associated; we get it and enter it for the cells.
+            .data(function(d) {
+                console.log(d);
+                return d;
+            })
+            .enter()
+            .append("td")
+            .text(function(d) {
+                return d;
+            });
+});
+
+d3.csv("csv/new_top_mentions.csv", function(error, myData) {
+    if (error) {
+        console.log("Had an error loading file.");
+    }
+    // We'll be using simpler data as values, not objects.
+    var myArray = [];
+    myData.forEach(function(d, i){
+        // Add a new array with the values of each:
+        myArray.push([d.Brand, d.Mentions, d.Tweets]);
+    });
+    console.log(myData);
+    console.log(myArray);
+    
+    // You could also have made the new array with a map function!
+    //using colors and fonts from the UNICEF Style Guide
+    var table = d3.select("#table2").append("table")
+    .attr("id", "keywords")
+    .attr("cellspacing", 0)
+    .attr("cellpadding", 0);
+    var header = table.append("thead").append("tr");
+    header
+            .selectAll("th")
+            .data(["Brands", "Mentions", "Tweets"])
+            .enter()
+            .append("th")
+            .append("span")
+            .text(function(d) { return d; });
+    var tablebody = table.append("tbody");
+    rows = tablebody
+            .selectAll("tr")
+            .data(myArray)
+            .enter()
+            .append("tr");
+    // We built the rows using the nested array - now each row has its own array.
+    cells = rows.selectAll("td")
+        // each row has data associated; we get it and enter it for the cells.
+            .data(function(d) {
+                console.log(d);
+                return d;
+            })
+            .enter()
+            .append("td")
+            .text(function(d) {
+                return d;
+            });
+});
